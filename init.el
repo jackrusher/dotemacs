b84379b2bc3920e9a5348bfce43176d0bbc6952f
Jack Rusher <jack@rusher.com>
2014-03-01 12:07:35 -0500

    Fix typo in user-local config, remove 03-packages

--- init.el
+++ init.el
@@ -1,7 +1,5 @@
 ;; -*- emacs-lisp -*-
 
-;;; NO SPLASH, NO CHROME
-
 ;; turn off splash screen messages
 (setq inhibit-startup-echo-area-message t
       inhibit-startup-screen t)
@@ -13,36 +11,40 @@
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
-;; I have loads of RAM, don't start GC until I've used a decent chunk of it.
+;; I have loads of RAM, don't start GC until I've used a decent chunk
+;; of it.
 (setq gc-cons-threshold 20000000)
 
 ;; I'll be sending files from the command line
 (server-start)
 
-;;; local lisp packages for this configuration lives here
+;;; local lisp packages for this configuration live here
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
 
-;;; LOAD MODULES
-;;; The same approach as /etc/rc.d, all startup filenames begin with a
-;;; number and get loaded in numerical order.
+;;; Use Cask for ELPA-style packages
+(require 'cask "~/.cask/cask.el")
+(cask-initialize)
 
+;;; Like /etc/rc.d, all startup filenames begin with a number and get
+;;; loaded in numerical order.
 (mapc #'load-file (directory-files (concat user-emacs-directory "modules") t "[0-9]*.el"))
 
 ;;; PER-USER CUSTOMIZATIONS
 
-;; A few people use this configuration, so here's a hook to load
-;; "~/.emacs.d/lisp/<username>-local.el" if such a file exists on the load path.
-;; Place personal extensions to this configuration in that file to
-;; avoid merge hassles later.
-(let ((per-user-file (concat user-emacs-directory (user-login-name) "-local.el")))
+;; Here's a hook to load "~/.emacs.d/lisp/<username>-local.el" if such
+;; a file exists on the load path.  Place personal extensions to this
+;; configuration in that file to avoid merge hassles later.
+(let ((per-user-file (concat (user-login-name) "-local.el")))
   (when (locate-library per-user-file) (load per-user-file)))
 
 ;; TODO bring in latex customizations from old .emacs
 
 ;; TODO bring in org-mode customizations from old .emacs, and add some
+
 ;; Mac OS X capture mode scripting for grabbing bits and pieces from
 ;; outside emacs (via Automator).
+
 ;;(setq org-default-notes-file (concat org-directory "/notes.org"))
 ;;(define-key global-map "<s-E>" 'org-capture)
 
-;; TODO bring in web development stuff
+;; TODO bring in more web development stuff
