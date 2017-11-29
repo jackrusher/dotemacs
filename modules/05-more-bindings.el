370d9e3d52a2776930b4b0c80c1f35dfccaa326a
Lauren Papot <dave.liepmann@gmail.com>
2017-11-29 13:50:27 +0100

    Multiple-cursors keybindings

--- modules/05-more-bindings.el
+++ modules/05-more-bindings.el
@@ -61,3 +61,9 @@
 
 ;; Enable which-key for function discovery
 (which-key-mode)
+
+;; Multiple cursors magic
+(global-set-key (kbd "C-,") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
