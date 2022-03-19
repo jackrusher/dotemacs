fab15c54eaaf100442fc456bb89e15638297b32a
Jack Rusher <jack@rusher.com>
2022-03-19 17:38:22 +0100

    Add early init file

---
+++ early-init.el
@@ -0,0 +1,34 @@
+;;; early-init.el -*- lexical-binding: t; -*-
+
+;; Emacs HEAD (27+) introduces early-init.el, which is run before init.el,
+;; before package and UI initialization happens.
+
+;; Defer garbage collection further back in the startup process
+;;(setq gc-cons-threshold most-positive-fixnum)
+;; XXX if this isn't fixed later the emacs process eventually eats all
+;; available memory.
+
+(setq gc-cons-threshold (* 256 1024 1024))
+
+;; Prevent the glimpse of un-styled Emacs by disabling these UI elements early.
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+;; Resizing the Emacs frame can be a terribly expensive part of changing the
+;; font. By inhibiting this, we easily halve startup times with fonts that are
+;; larger than the system default.
+(setq frame-inhibit-implied-resize t)
+
+;; Ignore X resources; its settings would be redundant with the other settings
+;; in this file and can conflict with later config (particularly where the
+;; cursor color is concerned).
+(advice-add #'x-apply-session-resources :override #'ignore)
+
+;; Prevent unwanted runtime builds; packages are compiled ahead-of-time when
+;; they are installed and site files are compiled when gccemacs is installed.
+(setq comp-deferred-compilation nil)
+
+;; turn off splash screen messages
+(setq inhibit-startup-echo-area-message t
+      inhibit-startup-screen t)
