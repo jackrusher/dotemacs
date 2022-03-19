ad56b4af91e8ae63586d40dd126cefc462e13681
Jack Rusher <jack@rusher.com>
2022-03-19 17:38:48 +0100

    general cleanup

--- init.el
+++ init.el
@@ -1,27 +1,4 @@
-;; -*- emacs-lisp -*-
-
-;; turn off splash screen messages
-(setq inhibit-startup-echo-area-message t
-      inhibit-startup-screen t)
-
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/")
-             '("org" . "http://orgmode.org/elpa/"))
-(when (version< emacs-version "27.0") (package-initialize))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(menu-bar-mode nil)
- '(package-selected-packages
-   (quote
-    (typescript-mode dired-details applescript-mode company-pollen pollen-mode edn gnuplot which-key org-babel-eval-in-repl htmlize ox-reveal ttl-mode sparql-mode yaml-mode visual-regexp undo-tree tuareg smex smartparens slime-company skewer-mode robe restclient rainbow-mode processing-mode pretty-symbols powerline parenface-plus paredit pallet motion-mode markdown-mode magit json-mode js2-refactor ido-ubiquitous haml-mode go-mode geiser fringe-helper flymake-ruby flymake-coffee find-file-in-project expand-region elisp-slime-nav diminish company-quickhelp company-inf-ruby company-ghc color-theme coffee-mode cider-eval-sexp-fu cider bundler aggressive-indent ag ack-and-a-half ace-jump-mode)))
- '(scroll-bar-mode nil)
- '(tool-bar-mode nil))
+;; -*- emacs-lisp -*- -*- lexical-binding: t; -*-
 
 ;;; utf-8 all the time
 (setq locale-coding-system 'utf-8)
@@ -30,9 +7,6 @@
 (set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 (setq slime-net-coding-system 'utf-8-unix)
-
-;; I'll be sending files from the command line
-(server-start)
 
 ;;; local lisp packages for this configuration live here
 (add-to-list 'load-path (concat user-emacs-directory "lisp"))
@@ -49,15 +23,8 @@
 (let ((per-user-file (concat (user-login-name) "-local.el")))
   (when (locate-library per-user-file) (load per-user-file)))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-scrollbar-bg ((t (:background "#2f2f2f"))))
- '(company-scrollbar-fg ((t (:background "#222222"))))
- '(company-tooltip ((t (:inherit default :background "#1b1b1b"))))
- '(company-tooltip-common ((t (:inherit font-lock-constant-face))))
- '(company-tooltip-common-selection ((t (:inherit font-lock-keyword-face))))
- '(company-tooltip-selection ((t (:inherit font-lock-keyword-face)))))
 (put 'narrow-to-region 'disabled nil)
+
+;; I'll be sending files from the command line
+(server-start)
+
